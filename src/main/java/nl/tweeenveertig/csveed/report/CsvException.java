72669dae165380b211fff31585ef13b9aef838a1
Robert Bor <bor.robert@gmail.com>
2013-03-02 20:42:13 +0100

    Removed unused code from CsvException

--- src/main/java/nl/tweeenveertig/csveed/report/CsvException.java
+++ src/main/java/nl/tweeenveertig/csveed/report/CsvException.java
@@ -20,30 +20,4 @@ public class CsvException extends RuntimeException {
         return this.error;
     }
 
-//    private RowReport report;
-//
-//    private int lineNumber;
-//
-//    public CsvException(String message) {
-//        super(message);
-//    }
-//
-//    public CsvException(String message, Throwable err) {
-//        super(message, err);
-//    }
-//
-//    public CsvException(String message, Throwable err, RowReport report, int lineNumber) {
-//        super(message, err);
-//        this.report = report;
-//        this.lineNumber = lineNumber;
-//    }
-//
-//    public RowReport getReport() {
-//        return this.report;
-//    }
-//
-//    public int getLineNumber() {
-//        return lineNumber;
-//    }
-
 }
