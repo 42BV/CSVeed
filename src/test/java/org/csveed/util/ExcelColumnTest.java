7bf202ca2c678d5b98f13936868d19267ab8b1a2
Robert Bor <bor.robert@gmail.com>
2013-03-16 11:31:27 +0100

    Added test for column index 0

--- src/test/java/org/csveed/util/ExcelColumnTest.java
+++ src/test/java/org/csveed/util/ExcelColumnTest.java
@@ -6,6 +6,7 @@ import static junit.framework.Assert.assertEquals;
 
 public class ExcelColumnTest {
 
+
     @Test
     public void excelColumnToColumnIndex() {
         ExcelColumn excel = new ExcelColumn("AH");
@@ -22,6 +23,12 @@ public class ExcelColumnTest {
     public void columnIndexToExcelColumn() {
         ExcelColumn excel = new ExcelColumn(34);
         assertEquals("AH", excel.getExcelColumn());
+    }
+
+    @Test
+    public void wrongIndex() {
+        ExcelColumn excel = new ExcelColumn(0);
+        assertEquals("", excel.getExcelColumn());
     }
 
 }
