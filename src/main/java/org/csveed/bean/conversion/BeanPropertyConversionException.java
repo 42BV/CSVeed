73de7603874c42f659e23e50f8526192b746913a
Jeremy Landis <jeremylandis@hotmail.com>
2015-01-04 19:12:42 -0500

    added default serialization.

--- src/main/java/org/csveed/bean/conversion/BeanPropertyConversionException.java
+++ src/main/java/org/csveed/bean/conversion/BeanPropertyConversionException.java
@@ -2,6 +2,8 @@ package org.csveed.bean.conversion;
 
 public class BeanPropertyConversionException extends ConversionException {
 
+    private static final long serialVersionUID = 1L;
+
     public BeanPropertyConversionException(String message, String propertyDescription, Throwable exception) {
         super(message, propertyDescription, exception);
     }
