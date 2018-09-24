2df25781769f786c49daf1ef385e8f987f4f7526
Jeremy Landis <jeremylandis@hotmail.com>
2018-09-23 20:22:16 -0400

    [ci] Remove unused imports

--- src/test/java/org/csveed/bean/BeanWriterTest.java
+++ src/test/java/org/csveed/bean/BeanWriterTest.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.csveed.test.model.BeanWithMultipleStrings;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class BeanWriterTest {
--- src/test/java/org/csveed/row/RowReaderTest.java
+++ src/test/java/org/csveed/row/RowReaderTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.List;
