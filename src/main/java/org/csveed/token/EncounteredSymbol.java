1b3c6e5d3e3b36656beb25fee0d23073cdbaede7
Jeremy Landis <jeremylandis@hotmail.com>
2023-06-11 16:55:58 -0400

    [ci] Don't double initialize

--- src/main/java/org/csveed/token/EncounteredSymbol.java
+++ src/main/java/org/csveed/token/EncounteredSymbol.java
@@ -14,9 +14,9 @@ public enum EncounteredSymbol {
     SPACE_SYMBOL, SEPARATOR_SYMBOL, QUOTE_SYMBOL(true), ESCAPE_SYMBOL(true), EOL_SYMBOL, EOL_SYMBOL_TRASH(false, true),
     OTHER_SYMBOL, BOM_SYMBOL(false, true), END_OF_FILE_SYMBOL, COMMENT_SYMBOL;
 
-    private boolean checkForSimilarEscapeAndQuote = false;
+    private boolean checkForSimilarEscapeAndQuote;
 
-    private boolean trash = false;
+    private boolean trash;
 
     private EncounteredSymbol() {
         this(false, false);
