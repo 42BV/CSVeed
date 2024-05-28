60c1c60d900cc6326c24d1148e41fa0bdb009540
Jeremy Landis <jeremylandis@hotmail.com>
2024-05-28 14:12:28 -0400

    [packages] modular requires packages match on source / test side, fixing so we have placeholders
    Tests are under a folder test that are unrelated to the source side, javadocs try to force modular and will not work if they do not include proper packaging so added these.  They serve no other purpose other than to fix the site generation due to release failing when javadocs cannot work here.

---
+++ src/main/java/org/csveed/test/converters/Placeholder.java
@@ -0,0 +1,20 @@
+/*
+ * CSVeed (https://github.com/42BV/CSVeed)
+ *
+ * Copyright 2013-2024 CSVeed.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of The Apache Software License,
+ * Version 2.0 which accompanies this distribution, and is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package org.csveed.test.converters;
+
+/**
+ * The Class Placeholder.
+ */
+public class Placeholder {
+
+    // Dummy placeholder to get around javadoc issues
+
+}
---
+++ src/main/java/org/csveed/test/converters/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * CSVeed (https://github.com/42BV/CSVeed)
+ *
+ * Copyright 2013-2024 CSVeed.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of The Apache Software License,
+ * Version 2.0 which accompanies this distribution, and is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+/**
+ * Placeholder to resolve javadoc requirements.
+ */
+package org.csveed.test.converters;
